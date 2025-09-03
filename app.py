6916848036f751bb1886c561642354088264ac9a
lnwrealx2-sys <lnwrealx2@gmail.com>
2025-09-03 21:06:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    return render_template('index.html')
+    return 'ควย'
 
 
 if __name__ == '__main__':
 
     app.run(debug=True)
+
 
 
 
