e96954693b2fce3aa5f1d27b8dab514c50fd9512
lnwrealx2-sys <lnwrealx2@gmail.com>
2025-09-03 20:59:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,jsonify
+from flask import Flask,render_template
 import os
 
 app = Flask(__name__)
@@ -21,5 +21,6 @@ def index():
 if __name__ == '__main__':
 
     app.run(debug=True)
+
 
 
